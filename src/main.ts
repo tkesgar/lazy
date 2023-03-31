9c5e393709ebf89789ae8d4e0224d07620004582
Ted Kesgar <t.kesgar@outlook.com>
2023-03-31 17:14:25 +0700

    Add docs

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,29 @@
-export type LazyValueOf<L> = L extends Lazy<infer T> ? T : never;
+type Fn<T> = () => T;
 
-export type LazyAsyncValueOf<L> = L extends LazyAsync<infer T> ? T : never;
-
+/**
+ * Wraps a function `fn` to be executed lazily:
+ *
+ * - `fn` will never be called until `.value` is accessed
+ * - Once `fn` is called and returns a value, the value is saved and `fn` will
+ *   never be called anymore.
+ *
+ * `fn` will be called again if `fn` returns `undefined` or throws an error.
+ *
+ * Note that `Lazy` does not work well with asynchronous functions (functions
+ * that return `Promise` objects), since the resulting promise may be rejected.
+ * Use `LazyAsync` to work with asynchronous functions.
+ */
 export class Lazy<T> {
-  readonly #fn: () => T;
+  readonly #fn: Fn<T>;
   #value: T | undefined;
 
-  constructor(fn: () => T) {
+  constructor(fn: Fn<T>) {
     this.#fn = fn;
   }
 
+  /**
+   * Returns `fn()` or the saved value.
+   */
   get value(): T {
     if (this.#value === undefined) {
       this.#value = this.#fn();
@@ -19,20 +33,54 @@ export class Lazy<T> {
   }
 }
 
-export function lazify<T>(fn: () => T) {
+/**
+ * Retrieves the value type of a `Lazy`.
+ *
+ * ```ts
+ * const myLazy = new Lazy(() => 'foo')
+ * type MyLazy = LazyValueOf<typeof myLazy> // string
+ * ```
+ */
+export type LazyValueOf<L> = L extends Lazy<infer T> ? T : never;
+
+/**
+ * Returns a lazy version of `fn`.
+ *
+ * @see {Lazy}
+ * @param fn Function to be wrapped
+ * @returns The lazy version of `fn`
+ */
+export function lazify<T>(fn: Fn<T>): Fn<T> {
   const lazy = new Lazy(fn);
   return () => lazy.value;
 }
 
+/**
+ * Wraps an asynchronous function `asyncFn` (function that returns a `Promise`)
+ * to be executed lazily:
+ *
+ * - `asyncFn` will never be called until `.getValue()` is called
+ * - Once `asyncFn` is called and resolves to a value, the value is saved and
+ * `asyncFn` will never be called anymore.
+ *
+ * `asyncFn` will be called again if `asyncFn` resolves to `undefined` or
+ * rejects with an error.
+ */
 export class LazyAsync<T> {
-  readonly #fn: () => Promise<T>;
+  readonly #fn: Fn<Promise<T>>;
   #value: T | undefined;
   #promise: Promise<T> | undefined;
 
-  constructor(fn: () => Promise<T>) {
-    this.#fn = fn;
+  constructor(asyncFn: Fn<Promise<T>>) {
+    this.#fn = asyncFn;
   }
 
+  /**
+   * Returns the saved value.
+   *
+   * If `getValue()` is never called before, accessing `.value` throws an
+   * error.
+   */
   get value(): T {
     if (this.#value === undefined) {
       throw new TypeError("Lazy async value is not initialized yet");
@@ -41,6 +89,11 @@ export class LazyAsync<T> {
     return this.#value;
   }
 
+  /**
+   * Resolves to `fn()`.
+   *
+   * @returns
+   */
   getValue(): Promise<T> {
     if (this.#promise === undefined) {
       this.#promise = (async () => {
@@ -58,7 +111,24 @@ export class LazyAsync<T> {
   }
 }
 
-export function lazifyAsync<T>(fn: () => Promise<T>) {
-  const lazy = new LazyAsync(fn);
+/**
+ * Retrieves the value type of a `LazyAsync`.
+ *
+ * ```ts
+ * const myLazy = new LazyAsync(async () => 'foo')
+ * type MyLazy = LazyAsyncValueOf<typeof myLazy> // string
+ * ```
+ */
+export type LazyAsyncValueOf<L> = L extends LazyAsync<infer T> ? T : never;
+
+/**
+ * Returns a lazy version of `asyncFn`.
+ *
+ * @see {LazyAsync}
+ * @param asyncFn Async function to be wrapped
+ * @returns The lazy version of `asyncFn`
+ */
+export function lazifyAsync<T>(asyncFn: Fn<Promise<T>>): Fn<Promise<T>> {
+  const lazy = new LazyAsync(asyncFn);
   return () => lazy.getValue();
 }
